32fca0c92a84daf475b969a2184876e34bac9822
quangtri.arena@gmail.com <quangtri.arena@gmail.com>
2023-03-12 23:33:35 +0700

    update source

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import cookieParser from 'cookie-parser'
 import cors from 'cors'
 import dotenv from 'dotenv'
 import express from 'express'
-import AdminRoutes from './src/routers/index.js'
+import AdminRoutes from './src/routers'
 dotenv.config()
 
 const app = express()
